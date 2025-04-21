c687ae5d982c84d16bee77c8a8f458b4bf5388a1
Pedro Fonseca <pedrofonseca447@gmail.com>
2025-04-21 18:55:28 -0300

    logica inicial

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import ticTacToe from './components/tic-tac-toe.vue';
+import Sudoku from './components/Sudoku.vue';
 </script>
 
 <template>
   <div>
-    <ticTacToe></ticTacToe>
+    <Sudoku></Sudoku>
   </div>
 </template>
 
